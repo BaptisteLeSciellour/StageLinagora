1cd3ba295f661bb3a7cd609b727eb0f8edde5409
BaptisteLeSciellour <bapt802@gmail.com>
2023-06-28 09:45:15 +0200

    Code pour me donner la liste de toutes les valeurs possibles d une case donné et me les écrires dans un tableau excel

---
+++ TypeTache.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+
+def main():
+    tableau = []
+    verif = False
+    with open("conver.txt",'r') as f:
+        for s in f:
+            verif = False
+            s=s.split(',')
+            for i in range(0,len(tableau)):
+                if(s[10]==tableau[i]):
+                    verif = True
+            if (verif == False):
+                tableau.append(s[10])
+
+    print(tableau)
+    df = pd.DataFrame(tableau)  # Créez un DataFrame à partir du tableau complet
+    df.to_excel("listeTypedeTache.xlsx", index=False, header=False, engine='openpyxl')  # Écrivez le DataFrame dans le fichier Excel
+
+
+if __name__ == "__main__":
+    main()
+
