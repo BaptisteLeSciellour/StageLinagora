d01c5a5685525fef623ad4436cfe0d013c0d0b61
BaptisteLeSciellour <bapt802@gmail.com>
2023-06-28 09:52:58 +0200

    ajout de commentaires

--- TypeTache.py
+++ TypeTache.py
@@ -8,7 +8,7 @@ def main():
             verif = False
             s=s.split(',')
             for i in range(0,len(tableau)):
-                if(s[10]==tableau[i]):
+                if(s[10]==tableau[i]): # c'est ici au niveau du chiffre qu'il faut changer, il faut mettre la valeur du tableau que vous souhaitez comparé
                     verif = True
             if (verif == False):
                 tableau.append(s[10])
